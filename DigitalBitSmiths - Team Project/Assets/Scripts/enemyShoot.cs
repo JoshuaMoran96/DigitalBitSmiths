9b005ceb7077ed07288dea620969be31d94acee6
flux116DEVX <flux116@gmail.com>
2026-05-12 12:50:14 -0400

    Created EnemyGunner, Bullet enemy(not players bullet) , added script for bullet enemy and enemyshoot

---
+++ DigitalBitSmiths - Team Project/Assets/Scripts/enemyShoot.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class enemyShoot : MonoBehaviour
+{
+    public GameObject enemyBulletPrefab;
+    public Transform firePoint;
+    public float fireRate = 1f;
+
+    float nextFireTime;
+
+    public Transform player;
+
+    void Update()
+    {
+        if (player == null)
+            return;
+
+        if (Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        Vector2 direction = player.position - firePoint.position;
+
+        float angle =
+            Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        Instantiate(
+            enemyBulletPrefab,
+            firePoint.position,
+            Quaternion.Euler(0f, 0f, angle)
+        );
+    }
+}
+
+//logic path for enemy gunner to fire at the player using its specific bullet
