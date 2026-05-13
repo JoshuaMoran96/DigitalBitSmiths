ef382e66b770fceb772ca29842ba2dfe099f56a9
JoshuaMoran96 <joshuafmoran@icloud.com>
2026-05-13 11:13:16 -0500

    Dash / iFrames
    Removed sprint function, turned it into a Dash function with iFrames that allow you to dodge bullets/enemy collisions.

--- DigitalBitSmiths - Team Project/Assets/Scripts/playerController.cs
+++ DigitalBitSmiths - Team Project/Assets/Scripts/playerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
@@ -10,18 +11,38 @@ public class playerController : MonoBehaviour, IDamage
     [SerializeField] Rigidbody2D rb;
     [SerializeField] CapsuleCollider2D capsuleCollider;
     //[SerializeField] Transform cam;
-  
+
+    private SpriteRenderer spriteRenderer;
+
+    [Header("Health")]
     [SerializeField] float currentHP;
     [SerializeField] float maxHP;
+
+
+    [Header("Movement")]
     [SerializeField] float speed;
     [SerializeField] float sprintSpeed;
     [SerializeField] int jumpHeight;
     [SerializeField] float groundCheckRadius = 0.2f;
     [SerializeField] float lerpSpeed = 5f;
     [SerializeField] SpriteRenderer faceDir; 
-    [SerializeField] int jumpCount; 
+    [SerializeField] int jumpCount;
+
+
+    // Dash Stats
+    [Header("Dash Settings")]
+    float dashTime;
+    public float dashSpeed = 20.0f;
+    public float dashDuration = 0.15f;
+    public float dashCooldown = 0.4f;
+    public bool canDash;
+    public bool isDashing;
+    public float iFrameDuration = 3.0f;
     
     public LayerMask groundLayer;
+
+    public LayerMask enemyLayer;
+    public LayerMask bulletLayer;
     //ublic Transform groundCheck;
 
     [SerializeField] float rayLength = 0.5f;
@@ -38,6 +59,11 @@ public class playerController : MonoBehaviour, IDamage
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        canDash = true;
+
         isGrounded = true; // player starting state will be grounded
         currentHP = 100; // start the player off with full hp
         rb = GetComponent<Rigidbody2D>(); // set the rb comp
@@ -47,13 +73,16 @@ public class playerController : MonoBehaviour, IDamage
         {
             healthImage = GameObject.Find("Health").GetComponent<Image>();
         }
+
+        
     }
 
     // Update is called once per frame
     void Update() // read input
     {
         getInput();
-        sprint();
+
+        //sprint();
         
         flipDir();
         updateHealthBar();
@@ -62,10 +91,14 @@ public class playerController : MonoBehaviour, IDamage
     private void FixedUpdate() // apply physics to rb using that input
     {
 
+        if (isDashing)
+            return;
+
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
         groundCheck();
 
-        Debug.Log("Grounded 1 " + isGrounded + " movement " + movDir.x);
+        //Debug.Log("Grounded 1 " + isGrounded + " movement " + movDir.x);
     }
 
     // flip dir 
@@ -100,13 +133,19 @@ public class playerController : MonoBehaviour, IDamage
     }
 
     // get input ()
-    void getInput() {
+    void getInput() 
+    {
         moveInput = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && (isGrounded || jumpCount > 0))
         {
             Jump();
 
+        }
+
+        if (Input.GetButtonDown("Sprint"))
+        {
+            dash();
         }
 
     }
@@ -151,22 +190,14 @@ public class playerController : MonoBehaviour, IDamage
             
     }
 
-
-    //Dash
-    public void dash()
-    {
-        
-    }
-
-
     // Sprint
-    public void sprint()
-    {
-        if (Input.GetButtonDown("Sprint"))
-            speed *= sprintSpeed;
-        else if (Input.GetButtonUp("Sprint"))
-            speed /= sprintSpeed;
-    }
+    //public void sprint()
+    //{
+    //    if (Input.GetButtonDown("Sprint"))
+    //        speed *= sprintSpeed;
+    //    else if (Input.GetButtonUp("Sprint"))
+    //        speed /= sprintSpeed;
+    //}
 
     // damage stuff
     public void takeDamage(float amount) {
@@ -182,6 +213,66 @@ public class playerController : MonoBehaviour, IDamage
     {
 
         takeDamage(amount);
+    }
+
+    //Dash
+    public void dash()
+    {
+        if (!canDash)
+            return;
+
+        canDash = false;
+        isDashing = true;
+
+        float dir = faceDir.flipX ? -1 : 1;
+
+        rb.linearVelocity = new Vector2(dir * dashSpeed, rb.linearVelocity.y);
+
+        dashTime = Time.time + dashDuration;
+
+        StartCoroutine(iFrames());
+
+        Invoke(nameof(endDash), dashDuration);
+
+        Invoke(nameof(resetDash), dashCooldown);
+
+    }
+
+    void endDash()
+    {
+        isDashing = false;
+    }
+
+    void resetDash()
+    {
+        canDash = true;
+    }
+
+    IEnumerator iFrames()
+    {
+        int enemyLayer = LayerMask.NameToLayer("Enemy");
+
+        int bulletLayer = LayerMask.NameToLayer("EnemyBullet");
+
+        Physics2D.IgnoreLayerCollision(gameObject.layer, enemyLayer, true);
+        Physics2D.IgnoreLayerCollision(gameObject.layer, bulletLayer, true);
+
+        SetOpacity(0.25f);
+
+        yield return new WaitForSeconds(iFrameDuration);
+
+        SetOpacity(1.0f);
+
+        Physics2D.IgnoreLayerCollision(gameObject.layer, enemyLayer, false);
+        Physics2D.IgnoreLayerCollision(gameObject.layer, bulletLayer, false);
+    }
+
+    public void SetOpacity(float alpha)
+    {
+        Color color = spriteRenderer.color;
+        color.a = Mathf.Clamp01(alpha);
+        spriteRenderer.color = color;
+
     }
 
 }
