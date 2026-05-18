43d62ea0ace879e73724375054a18d7751ca321f
flux116DEVX <flux116@gmail.com>
2026-05-18 12:39:01 -0400

    Created moving platform script and platform prefab with the first three components of platform, point 0 and point1. Player can jump on and move with the platform having complete player control. Pltaform can be set on a track in any direction between points using a index of origin to destination. Adding to the track will require adding more point locations onto platform object but can scale quite easily with needs. Duplication of prefab in hierachy and you can assemble multiple moving platforms and set varying speed and routes.

---
+++ DigitalBitSmiths - Team Project/Assets/Scripts/Moving Platform.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    public int startingPoint;
+    public Transform[] points;
+    private int i;
+
+    private Vector2 previousPosition;
+    private Vector2 platformVelocity;
+    private List<Rigidbody2D> passengers = new List<Rigidbody2D>();
+
+    void Start()
+    {
+        transform.position = points[startingPoint].position;
+        previousPosition = transform.position;
+        i = startingPoint; // Starts track movement from the correct starting index
+    }
+
+    // Changed to FixedUpdate 
+    void FixedUpdate()
+    {
+        // Move the platform
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++;
+            if (i == points.Length)
+            {
+                i = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.fixedDeltaTime);
+
+        //Calculate actual velocity of the platform this frame
+        platformVelocity = ((Vector2)transform.position - previousPosition) / Time.fixedDeltaTime;
+        previousPosition = transform.position;
+
+        // Move passengers along with the platform smoothly without locking their controls
+        foreach (Rigidbody2D passenger in passengers)
+        {
+            if (passenger != null)
+            {
+                passenger.position += platformVelocity * Time.fixedDeltaTime;
+            }
+        }
+    }
+
+    // Track when objects land on the platform
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+        if (rb != null && !passengers.Contains(rb))
+        {
+            passengers.Add(rb);
+        }
+    }
+
+    // Track when objects leave the platform
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+        if (rb != null && passengers.Contains(rb))
+        {
+            passengers.Remove(rb);
+        }
+    }
+}
+
