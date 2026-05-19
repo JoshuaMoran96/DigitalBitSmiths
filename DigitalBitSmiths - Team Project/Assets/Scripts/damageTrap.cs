0decd0400b9960fb0db6fdb4e7f528bfe3d7c484
Klaus <kerwin.jackson14@gmail.com>
2026-05-18 20:49:04 -0700

    Added Mine Trap
    Prefab uses Particle, Collider, explosion effect in Inspector.

--- DigitalBitSmiths - Team Project/Assets/Scripts/damageTrap.cs
+++ DigitalBitSmiths - Team Project/Assets/Scripts/damageTrap.cs
@@ -1,13 +1,59 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
+
+
 public class damageTrap : MonoBehaviour
 {
+    [SerializeField] SpriteRenderer sMine;
+    [SerializeField] GameObject trapType;
+    [SerializeField] explosion explosionEffect;
+    [SerializeField] CircleCollider2D circleCollider;
+
     [SerializeField] float damageAmount = 10.0f;
     [SerializeField] float damageRate = 1.0f; // Seconds between hits
+    [SerializeField] float flashTime = 0.1f;
 
+    [SerializeField] float riseTargetY = 0.7f;   // mine rise position
+    [SerializeField] float riseSpeed = 5f;       //how fast it rises
+
+    float startY;
     bool isDamaging;
+    bool isTriggered = false;    
+    bool hasExploded = false;  
 
-    private void OnTriggerStay2D(Collider2D other)
+    Color originalColor;
+    private void Start()
+    {
+        if (trapType == null) trapType = gameObject;
+
+        if (sMine == null)
+            sMine = GetComponent<SpriteRenderer>();
+
+        trapType = GameObject.Find(gameObject.name);
+
+        sMine.color = originalColor;
+        startY = trapType.transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        if (trapType.name != "Mine" || !isTriggered || hasExploded)
+            return;
+
+        // rise to target
+        float newY = Mathf.Lerp(trapType.transform.position.y, riseTargetY, Time.deltaTime * riseSpeed);
+        trapType.transform.position = new Vector2(trapType.transform.position.x, newY);
+
+        // check how close to target
+        if (Mathf.Abs(trapType.transform.position.y - riseTargetY) < 0.01f)
+        {
+            hasExploded = true;
+            BlastRadius();
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
     {
         //fix for spike traps working on enemies
         if (!other.CompareTag("Player"))
@@ -23,8 +69,12 @@ public class damageTrap : MonoBehaviour
         {
             StartCoroutine(ApplyDamage(dmg));
         }
+        if (trapType.name == "Mine" && !isTriggered)
+        {
+            isTriggered = true;
+            StartCoroutine(FlashRed());
+        }
     }
-
     IEnumerator ApplyDamage(IDamage target)
     {
         isDamaging = true;
@@ -36,4 +86,19 @@ public class damageTrap : MonoBehaviour
         isDamaging = false;
     }
 
+    // Flash for mine
+    IEnumerator FlashRed()
+    {
+        sMine.color = Color.red;
+        float newFlashTime = Mathf.Clamp(flashTime, 0.1f, 0.3f);
+        yield return new WaitForSeconds(newFlashTime);
+
+        sMine.color = originalColor;
+    }
+    void BlastRadius() {
+        circleCollider.radius = 2.0f;
+        if (explosionEffect != null)
+            Instantiate(explosionEffect, trapType.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
 }
